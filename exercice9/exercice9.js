ece1b6ed6807f2f187a9ba8168d8d9d9ae4841e5
AlexandrMardari <2266383@cegepoutaouais.qc.ca>
2024-11-02 20:29:46 -0400

    exercice 9 fait

---
+++ exercice9/exercice9.js
@@ -0,0 +1,17 @@
+let BtnDemmarerCompteRebours = document.getElementById("demarrerCompteReboursBtn");
+
+
+let div_temps = document.getElementById("compteRebours");
+
+div_temps =
+
+
+function demarrageCompteRebours(){
+
+}
+
+BtnDemmarerCompteRebours.addEventListener("click", demarrageCompteRebours);
+
+/*
+Je ne comprend pas ce numéro
+*/
